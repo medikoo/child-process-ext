7054442a441e8af26157c9ff5884832b9bceedbc
Mariusz Nowak <medyk@medikoo.com>
2019-01-10 12:00:56 +0100

    test: refactor

--- test/spawn.js
+++ test/spawn.js
@@ -10,69 +10,79 @@ const playgroundPath = resolve(__dirname, "_playground");
 const throwUnexpected = () => { throw new Error("Unexpected"); };
 
 describe("spawn", () => {
-	let successProgram, errorProgram, invalidProgram;
-	before(() => {
-		successProgram = spawn("./test-bin-success.js", ["foo", "--elo", "marko"], {
-			cwd: playgroundPath
-		});
-		errorProgram = spawn("./test-bin-error.js", ["umpa", "--elo", "marko"], {
-			cwd: playgroundPath
-		});
-		invalidProgram = spawn("./test-bin-non-existing.js", ["umpa", "--elo", "marko"], {
-			cwd: playgroundPath
-		});
-		return Promise.all([
-			successProgram.catch(noop), errorProgram.catch(noop), invalidProgram.catch(noop)
-		]);
+	describe("Successful execution", () => {
+		let program;
+		before(() =>
+			(program = spawn("./test-bin-success.js", ["foo", "--elo", "marko"], {
+				cwd: playgroundPath
+			})).catch(noop)
+		);
+
+		it("Should fulfill successfully", () => program.then());
+
+		it("Arguments should be passed into process", () =>
+			program.then(({ stdoutBuffer }) =>
+				assert.deepEqual(JSON.parse(stdoutBuffer).slice(2), ["foo", "--elo", "marko"])
+			)
+		);
+
+		it("Promise result should expose exit code", () =>
+			program.then(({ code }) => assert.equal(code, 0))
+		);
+
+		it("Promise result should expose stdout buffer", () =>
+			program.then(({ stdoutBuffer }) =>
+				assert.deepEqual(JSON.parse(stdoutBuffer).slice(2), ["foo", "--elo", "marko"])
+			)
+		);
+
+		it("Promise result should expose stderr", () =>
+			program.then(({ stderrBuffer }) => assert.equal(String(stderrBuffer), "stderr"))
+		);
 	});
 
-	it("Success exection should fulfill successfully", () => successProgram.then());
+	describe("Errorneous execution", () => {
+		let program;
+		before(() =>
+			(program = spawn("./test-bin-error.js", ["umpa", "--elo", "marko"], {
+				cwd: playgroundPath
+			})).catch(noop)
+		);
 
-	it("Arguments should be passed into process", () =>
-		successProgram.then(({ stdoutBuffer }) =>
-			assert.deepEqual(JSON.parse(stdoutBuffer).slice(2), ["foo", "--elo", "marko"])
-		)
-	);
+		it("Errorneous execution should resolve with rejection", () =>
+			program.then(throwUnexpected, noop)
+		);
 
-	it("Promise result should expose exit code", () =>
-		successProgram.then(({ code }) => assert.equal(code, 0))
-	);
+		it("Errorneous execution result should expose exit code", () =>
+			program.then(throwUnexpected, ({ code }) => assert.equal(code, 3))
+		);
 
-	it("Promise result should expose stdout buffer", () =>
-		successProgram.then(({ stdoutBuffer }) =>
-			assert.deepEqual(JSON.parse(stdoutBuffer).slice(2), ["foo", "--elo", "marko"])
-		)
-	);
+		it("Errorneous execution result should expose stdout", () =>
+			program.then(throwUnexpected, ({ stdoutBuffer }) =>
+				assert.equal(String(stdoutBuffer), "stdout")
+			)
+		);
 
-	it("Promise result should expose stderr", () =>
-		successProgram.then(({ stderrBuffer }) => assert.equal(String(stderrBuffer), "stderr"))
-	);
+		it("Errorneous execution result should expose stderr", () =>
+			program.then(throwUnexpected, ({ stderrBuffer }) =>
+				assert.equal(String(stderrBuffer), "stderr")
+			)
+		);
+	});
 
-	it("Errorneous execution should resolve with rejection", () =>
-		errorProgram.then(throwUnexpected, noop)
-	);
+	describe("Invalid execution", () => {
+		let program;
+		before(() =>
+			(program = spawn("./test-bin-non-existing.js", ["umpa", "--elo", "marko"], {
+				cwd: playgroundPath
+			})).catch(noop)
+		);
+		it("Invalid program execution should resolve with rejection", () =>
+			program.then(throwUnexpected, noop)
+		);
 
-	it("Errorneous execution result should expose exit code", () =>
-		errorProgram.then(throwUnexpected, ({ code }) => assert.equal(code, 3))
-	);
-
-	it("Errorneous execution result should expose stdout", () =>
-		errorProgram.then(throwUnexpected, ({ stdoutBuffer }) =>
-			assert.equal(String(stdoutBuffer), "stdout")
-		)
-	);
-
-	it("Errorneous execution result should expose stderr", () =>
-		errorProgram.then(throwUnexpected, ({ stderrBuffer }) =>
-			assert.equal(String(stderrBuffer), "stderr")
-		)
-	);
-
-	it("Invalid program execution should resolve with rejection", () =>
-		invalidProgram.then(throwUnexpected, noop)
-	);
-
-	it("Invalid program rejection should expose expected eror code", () =>
-		invalidProgram.then(throwUnexpected, ({ code }) => assert.equal(code, "ENOENT"))
-	);
+		it("Invalid program rejection should expose expected eror code", () =>
+			program.then(throwUnexpected, ({ code }) => assert.equal(code, "ENOENT"))
+		);
+	});
 });
