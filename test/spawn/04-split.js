1b319333f2e12a193d99aa588798662592746b27
Mariusz Nowak <medyk@medikoo.com>
2019-01-11 15:24:15 +0100

    feat: "split" option

---
+++ test/spawn/04-split.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const { resolve } = require("path")
+    , { assert }  = require("chai")
+    , spawn       = require("../../spawn");
+
+const playgroundPath = resolve(__dirname, "_playground");
+
+describe("spawn - Split stdout", () => {
+	let program;
+	const stdoutLines = [], stderrLines = [];
+	before(() => {
+		program = spawn("./test-bin-split.js", null, { cwd: playgroundPath, split: true });
+		program.stdout.on("data", data => stdoutLines.push(data));
+		program.stderr.on("data", data => stderrLines.push(data));
+		return program;
+	});
+
+	it("Should split stdout", () =>
+		assert.deepEqual(stdoutLines, ["firstBLAline", "secondBLAline", "", "fourthBLAline"])
+	);
+
+	it("Should split stderr", () => assert.deepEqual(stderrLines, ["one"]));
+});
