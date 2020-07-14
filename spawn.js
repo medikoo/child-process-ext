74b9c584893b77e9e0d9db84014cf8288bb09acd
Mariusz Nowak <medyk@medikoo.com>
2020-07-14 13:31:10 +0200

    tmp

--- spawn.js
+++ spawn.js
@@ -9,7 +9,7 @@ const ensureString = require("es5-ext/object/validate-stringifiable-value")
     , ensureObject = require("es5-ext/object/valid-object")
     , mixin        = require("es5-ext/object/mixin")
     , log          = require("log").get("child-process-ext:spawn")
-    , spawn        = require("cross-spawn")
+    , { spawn }    = require("child_process")
     , setupStd     = require("./lib/private/spawn/setup-std");
 
 const stdinLog = log.get("std:in");
